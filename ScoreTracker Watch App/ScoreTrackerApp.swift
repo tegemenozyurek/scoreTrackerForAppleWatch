95cae462216dad9ac2ab9e612940ea436d2ec771
egemen <egemenozyurek@outlook.com>
2025-08-30 18:53:38 +0300

    Initial Commit

---
+++ ScoreTracker Watch App/ScoreTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTrackerApp.swift
+//  ScoreTracker Watch App
+//
+//  Created by Turgut Egemen Özyürek on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreTracker Watch AppTests/ScoreTracker_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreTracker_Watch_AppTests.swift
+//  ScoreTracker Watch AppTests
+//
+//  Created by Turgut Egemen Özyürek on 30.08.2025.
+//
+
+import Testing
+@testable import ScoreTracker_Watch_App
+
+struct ScoreTracker_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
